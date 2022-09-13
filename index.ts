d58dd98b8767257d7be50609c7043c3a2cf619d9
elleswift <105027856+elleswift@users.noreply.github.com>
2022-09-12 20:46:16 -0400

    updates

--- index.ts
+++ index.ts
@@ -29,6 +29,34 @@ function findIndex(index: number,arr:number[]): number {
   return arr[index]
 }
 
+function log(msg: string| number): void {
+  // inserts into the database **** does not return anything ----- console.log('hurray...database inserted')
+  console.log(msg) 
+
+}
+
 // console.log(findIndex(1,NumberOfStudents))
 let finalNumber: number = findIndex(1,NumberOfStudents)
-console.log(finalNumber + 1)
+// console.log(finalNumber + 1)
+// log(finalNumber +1)
+
+const complexObject : any = {name: 'damian', height: 'yes please', age: 49}
+
+interface Human {
+  name: string,
+  height: string,
+}
+
+interface Teacher extends Human {
+  age: number,
+  weight?: string
+
+}
+// const complexObject2 : human  = {name: 'damian', height: 'yes please', age: 49}
+const complexObject2 : Teacher  = {name: 'damian', height: 'yes please', age: 49, weight: "200 ounces"}
+
+function displayteacher(teacher: Teacher): string {
+  return `this teach is ${teacher.age} old`
+}
+
+console.log(displayteacher(complexObject2))
